c53cf6e664663f54f9a41aeb59705cb00dbec231
kenli0n2323 <152817034+kli0n2323@users.noreply.github.com>
2026-03-21 16:10:51 -0500

    Fixed norm logic and added support for complex amps

--- main.py
+++ main.py
@@ -6,23 +6,23 @@ def build_state():
     zero = [1,0]
     one = [0,1]
     # - only two state real num qubits for now
-    amp0 = float(input("- [ Enter the amplitude for |0>: ] "))
-    amp1 = float(input("- [ Enter the amplitude for |1>: ] "))
+    amp0 = complex(input("- [ Enter the amplitude for |0>: ] "))
+    amp1 = complex(input("- [ Enter the amplitude for |1>: ] "))
     gen_state = [amp0, amp1]
     return gen_state
 
 
 # ---- ACTION FUNCTIONS ----
 def check_state_validity(gen_state):
-    norm_zero = np.abs(gen_state[0]) * np.abs(gen_state[0])
-    norm_one = np.abs(gen_state[1]) * np.abs(gen_state[1])
-    validity = norm_zero + norm_one
-    validity = round(validity, 1)
+    norm_zero = np.abs(gen_state[0]) ** 2
+    norm_one = np.abs(gen_state[1]) ** 2
+    norm = norm_zero + norm_one
+    validity = np.abs(norm - 1)
 
-    if validity == 1.0:
-        print(f"[ VALID ] | Normalizes to {validity}")
+    if validity < 10**(-4):
+        print(f"[ VALID ] | Normalizes to {norm}")
     else:
-        print(f"[ INVALID ] | Normalizes to {validity}")
+        print(f"[ INVALID ] | Normalizes to {norm}")
 
 
 # ---- WRAPPER FUNCTIONS ----
